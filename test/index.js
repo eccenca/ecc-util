0747cdf02a1be94e1b44ea7b21fcf7a52b579006
Lukas Eipert <lukas.eipert@eccenca.com>
2016-12-09 11:53:00 +0100

    add uuid function

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@
 import should from 'should';
 import sinon from 'sinon';
 
-import {changeFavicon, getBestLocale, getBrowserLocales} from '../index';
+import {changeFavicon, getBestLocale, getBrowserLocales, uuid} from '../index';
 
 // main test suite
 describe('changeFavicon', () => {
@@ -155,7 +155,11 @@ describe('getBestLocale', () => {
     });
 
     it('should add the defaultLocale to supportedLocales', () => {
-        should(getBestLocale({supportedLocales: ['en-au'], preferredLocales: ['en-US'], defaultLocale: 'en-US'})).eql('en-US');
+        should(getBestLocale({
+            supportedLocales: ['en-au'],
+            preferredLocales: ['en-US'],
+            defaultLocale: 'en-US'
+        })).eql('en-US');
     });
 
     it('should throw an Error if defaultLocale parameter is not a string', () => {
@@ -169,6 +173,27 @@ describe('getBestLocale', () => {
         }).should.throw(/Invalid argument/);
 
 
+    });
+
+});
+
+describe('uuid', () => {
+
+    it('should generate a uuid matching the uuid scheme', () => {
+        should(uuid()).match(/[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12}/);
+    });
+
+    it('should generate a fixed uuid with the same seed', () => {
+        should(uuid({
+            random: [
+                0x10, 0x91, 0x56, 0xbe, 0xc4, 0xfb, 0xc1, 0xea,
+                0x71, 0xb4, 0xef, 0xe1, 0x67, 0x1c, 0x58, 0x36
+            ]
+        })).equal('109156be-c4fb-41ea-b1b4-efe1671c5836');
+    });
+
+    it('should generate two different uuids in consecutive runs', () => {
+        should(uuid()).not.equal(uuid());
     });
 
 });
