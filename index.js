627bde4adcbd0d940e1c8a519ab3bdaec9c39c77
Lukas Eipert <lukas.eipert@eccenca.com>
2017-01-10 08:57:53 +0100

    add URI.js Wrapper

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
-import uuid from 'uuid/v4'
+import uuid from 'uuid/v4';
+import URI from './src/uri';
 
 export {uuid};
 export {changeFavicon} from './src/changeFavicon';
 export {getBestLocale, getBrowserLocales} from './src/locales';
+export {URI};
